3c95d7b755b0eed10a57c389ce2f7b322cd9f086
vylee2201 <161819486+vylee2201@users.noreply.github.com>
2025-03-14 00:37:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("📂 Upload File App")
+
+uploaded_file = st.file_uploader("Chọn một file CSV", type=["csv"])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write("📊 Xem trước dữ liệu:")
+    st.dataframe(df)
